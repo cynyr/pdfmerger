fa8c6bd540dd0bdbce8e22ded1bdfd5c7db60f40
Cynyr <andrew.frink@gmail.com>
2011-08-16 23:17:58 -0500

    Some doc strings, and readbility cleanups

--- pdfmerger.py
+++ pdfmerger.py
@@ -22,6 +22,7 @@ def do_help():
     sys.exit(1)
 
 def merge_pdfs(output_name, files):
+    """ Merges files in the order given. Make sure to sort first."""
     output = PdfFileWriter()
     for f in files:
         try:
@@ -53,9 +54,11 @@ def walk_paths(top, ctime):
     for root, dirs, files in os.walk(top, topdown=False):
         out_name = "%s %s.pdf" %(root,ctime)
         files = [npath(root,x) for x in files if bool(prog.match(x))]
+        files.sort()
         merge_pdfs(out_name, files)
 
 def check_path(p, cwd):
+    """make_abs would be a better name for this"""
     if not path.isabs(p):
         p = path.normpath(path.join(cwd,p))
     return p
@@ -66,6 +69,7 @@ def main(things):
     cwd = path.split(path.commonprefix(things))[0]
     if cwd =="":
         cwd = os.getcwd()
+    """Get the date code to use on merged files."""
     ctime = strftime("%Y%m%d%H%M%S", localtime())
     for thing in things:
         if path.isfile(thing) and bool(prog.match(thing)):
@@ -90,55 +94,3 @@ if __name__ == "__main__":
     if len(sys.argv) == 1 or "-h" in sys.argv:
         do_help()
     main(sys.argv[1:])
-
-            
-
-"""
-for root, dirs, files in os.walk(top, topdown=False):
-    for name in files:
-        os.remove(os.path.join(root, name))
-    for name in dirs:
-        os.rmdir(os.path.join(root, name))
-"""
-
-
-"""
-from pyPdf import PdfFileWriter, PdfFileReader
-
-output = PdfFileWriter()
-input1 = PdfFileReader(file("document1.pdf", "rb"))
-
-# print the title of document1.pdf
-print "title = %s" % (input1.getDocumentInfo().title)
-
-# add page 1 from input1 to output document, unchanged
-output.addPage(input1.getPage(0))
-
-# add page 2 from input1, but rotated clockwise 90 degrees
-output.addPage(input1.getPage(1).rotateClockwise(90))
-
-# add page 3 from input1, rotated the other way:
-output.addPage(input1.getPage(2).rotateCounterClockwise(90))
-# alt: output.addPage(input1.getPage(2).rotateClockwise(270))
-
-# add page 4 from input1, but first add a watermark from another pdf:
-page4 = input1.getPage(3)
-watermark = PdfFileReader(file("watermark.pdf", "rb"))
-page4.mergePage(watermark.getPage(0))
-
-# add page 5 from input1, but crop it to half size:
-page5 = input1.getPage(4)
-page5.mediaBox.upperRight = (
-    page5.mediaBox.getUpperRight_x() / 2,
-    page5.mediaBox.getUpperRight_y() / 2
-)
-output.addPage(page5)
-
-# print how many pages input1 has:
-print "document1.pdf has %s pages." % input1.getNumPages()
-
-# finally, write "output" to document-output.pdf
-outputStream = file("document-output.pdf", "wb")
-output.write(outputStream)
-outputStream.close()
-"""
